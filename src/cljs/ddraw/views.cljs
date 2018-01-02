52078f7f7d3d136ba9cc8d9e05f34c5896e6ff03
Joshua Glover <joshua.glover@klarna.com>
2018-01-02 11:59:49 +0100

    Inline styles are so stylish!

--- src/cljs/ddraw/views.cljs
+++ src/cljs/ddraw/views.cljs
@@ -11,10 +11,15 @@
         listening? (rf/subscribe [::subs/listening?])
         queue-created? (rf/subscribe [::subs/queue-created?])
         shapes (rf/subscribe [::subs/shapes])
-        current-shape (rf/subscribe [::subs/current-shape])]
+        current-shape (rf/subscribe [::subs/current-shape])
+        toggle-input (fn [shape-type]
+                       (rf/dispatch [::events/input-shape
+                                     (if (= shape-type (:type @current-shape))
+                                       nil
+                                       shape-type)]))]
     (if @authenticated?
       (do
-        [:div
+        [:div {:style {:width 640, :display "flex", :flex-direction "column", :justify-content "center"}}
          [:svg {:width 640
                 :height 480}
           (shapes/rectangle [0 0] 640 480 "lightgray")
@@ -23,25 +28,29 @@
                               (vec (concat [shape (assoc attrs :key i)] body)))))]
          (if @queue-created?
            [:div
-            [:div
+            [:div {:style {:display "flex", :margin-top 5}}
+             [widgets/button {:style {:margin-left 0}} #(toggle-input :rectangle) "Rectangle"]
+             [widgets/button {:style {:margin-left 5}} #(toggle-input :circle) "Circle"]
+             [widgets/button {:style {:margin-left 5}} #(toggle-input :triangle) "Triangle"]
+             [widgets/button {:style {:margin-left 5}} #(toggle-input :text) "Text"]
+             [widgets/button {:style {:margin-left "auto"}}
+              #(do
+                 (rf/dispatch [::events/clear-shapes])
+                 (rf/dispatch [::events/publish-command :clear])
+                 (rf/dispatch [::events/input-shape nil]))
+              "Clear shapes"]]
+            (when @current-shape
+              [:div {:style {:margin-top 5}}
+               (case (:type @current-shape)
+                 :circle [widgets/circle-input]
+                 :rectangle [widgets/rectangle-input]
+                 :triangle [widgets/triangle-input]
+                 :text [widgets/text-input]
+                 nil)])
+            [:div {:style {:display "flex", :flex-direction "row-reverse", :margin-top 5}}
              (if @listening?
                [widgets/button #(rf/dispatch [::events/stop-listening]) "Stop processing queue"]
-               [widgets/button #(rf/dispatch [::events/start-listening]) "Start processing queue"])
-             [widgets/button #(do
-                                (rf/dispatch [::events/clear-shapes])
-                                (rf/dispatch [::events/publish-command :clear]))
-              "Clear shapes"]]
-            [:div
-             [widgets/button #(rf/dispatch [::events/input-shape :rectangle]) "Rectangle"]
-             [widgets/button #(rf/dispatch [::events/input-shape :circle]) "Circle"]
-             [widgets/button #(rf/dispatch [::events/input-shape :triangle]) "Triangle"]
-             [widgets/button #(rf/dispatch [::events/input-shape :text]) "Text"]]
-            (case (:type @current-shape)
-              :circle [widgets/circle-input]
-              :rectangle [widgets/rectangle-input]
-              :triangle [widgets/triangle-input]
-              :text [widgets/text-input]
-              nil)]
+               [widgets/button #(rf/dispatch [::events/start-listening]) "Start processing queue"])]]
            (do
              (rf/dispatch [::events/create-queue!])
              [:div "Creating queue"]))])
--- src/cljs/ddraw/widgets.cljs
+++ src/cljs/ddraw/widgets.cljs
@@ -159,8 +159,11 @@
    "lightseagreen"
    "lightyellow"])
 
-(defn button [on-click-fn label]
-  [:button {:on-click on-click-fn} label])
+(defn button
+  ([on-click-fn label]
+   (button {} on-click-fn label))
+  ([attrs on-click-fn label]
+   [:button (merge attrs {:on-click on-click-fn}) label]))
 
 (defn color-picker []
   [:select
@@ -173,6 +176,9 @@
            :size 1
            :on-change #(rf/dispatch [::events/assoc-shape k (read-string (get-value %))])}])
 
+(defn span [style text]
+  [:span {:style style} text])
+
 (defn login-form []
   (let [username (r/atom nil)
         password (r/atom nil)]
@@ -181,64 +187,104 @@
      "Password:" [:input {:type "password", :on-change #(reset! password (get-value %))}]
      (button #(rf/dispatch [::events/login! @username @password]) "Login")]))
 
+(defn dispatch-shape [shape]
+  (rf/dispatch [::events/add-shape shape])
+  (rf/dispatch [::events/publish-command shape])
+  (rf/dispatch [::events/input-shape nil]))
+
 (defn circle-input []
   (let [shape (rf/subscribe [::subs/current-shape])]
-    [:div
-     "x" [num-input :x]
-     "y" [num-input :y]
-     "radius" [num-input :radius]
-     "color" [color-picker]
-     (button #(let [{:keys [x y radius color]} @shape]
-                (let [shape (shapes/circle [x y] radius color)]
-                  (rf/dispatch [::events/add-shape shape])
-                  (rf/dispatch [::events/publish-command shape])
-                  (rf/dispatch [::events/input-shape nil])))
-             "Add")]))
+    [:div {:style {:display "flex"}}
+     [span {:margin-right 5} "x"]
+     [num-input :x]
+
+     [span {:margin-left 5, :margin-right 5} "y"]
+     [num-input :y]
+
+     [span {:margin-left 5, :margin-right 5} "radius"]
+     [num-input :radius]
+
+     [span {:margin-left 5, :margin-right 5} "color"]
+     [color-picker]
+
+     [button {:style {:margin-left "auto"}}
+      #(let [{:keys [x y radius color]} @shape]
+         (dispatch-shape (shapes/circle [x y] radius color)))
+      "Add"]]))
 
 (defn rectangle-input []
   (let [shape (rf/subscribe [::subs/current-shape])]
-    [:div
-     "x" [num-input :x]
-     "y" [num-input :y]
-     "width" [num-input :width]
-     "height" [num-input :height]
-     "color" [color-picker]
-     (button #(let [{:keys [x y width height color]} @shape]
-                (let [shape (shapes/rectangle [x y] width height color)]
-                  (rf/dispatch [::events/add-shape shape])
-                  (rf/dispatch [::events/publish-command shape])
-                  (rf/dispatch [::events/input-shape nil])))
-             "Add")]))
+    [:div {:style {:display "flex"}}
+     [span {:margin-right 5} "x"]
+     [num-input :x]
+
+     [span {:margin-left 5, :margin-right 5} "y"]
+     [num-input :y]
+
+     [span {:margin-left 5, :margin-right 5} "width"]
+     [num-input :width]
+
+     [span {:margin-left 5, :margin-right 5} "height"]
+     [num-input :height]
+
+     [span {:margin-left 5, :margin-right 5} "color"]
+     [color-picker]
+
+     [button {:style {:margin-left "auto"}}
+      #(let [{:keys [x y width height color]} @shape]
+         (dispatch-shape (shapes/rectangle [x y] width height color)))
+      "Add"]]))
 
 (defn triangle-input []
   (let [shape (rf/subscribe [::subs/current-shape])]
-    [:div
-     "x1" [num-input :x1]
-     "y1" [num-input :y1]
-     "x2" [num-input :x2]
-     "y2" [num-input :y2]
-     "x3" [num-input :x3]
-     "y3" [num-input :y3]
-     "color" [color-picker]
-     (button #(let [{:keys [x1 y1 x2 y2 x3 y3 color]} @shape]
-                (let [shape (shapes/triangle [x1 y1] [x2 y2] [x3 y3] color)]
-                  (rf/dispatch [::events/add-shape shape])
-                  (rf/dispatch [::events/publish-command shape])
-                  (rf/dispatch [::events/input-shape nil])))
-             "Add")]))
+    [:div {:style {:display "flex"}}
+     [span {:margin-right 5} "x1"]
+     [num-input :x1]
+
+     [span {:margin-left 5, :margin-right 5} "y1"]
+     [num-input :y1]
+
+     [span {:margin-left 5, :margin-right 5} "x2"]
+     [num-input :x2]
+
+     [span {:margin-left 5, :margin-right 5} "y2"]
+     [num-input :y2]
+
+     [span {:margin-left 5, :margin-right 5} "x3"]
+     [num-input :x3]
+
+     [span {:margin-left 5, :margin-right 5} "y3"]
+     [num-input :y3]
+
+     [span {:margin-left 5, :margin-right 5} "color"]
+     [color-picker]
+
+     [button {:style {:margin-left "auto"}}
+      #(let [{:keys [x1 y1 x2 y2 x3 y3 color]} @shape]
+         (dispatch-shape (shapes/triangle [x1 y1] [x2 y2] [x3 y3] color)))
+      "Add"]]))
 
 (defn text-input []
   (let [shape (rf/subscribe [::subs/current-shape])]
-    [:div
-     "x" [num-input :x]
-     "y" [num-input :y]
-     "size" [num-input :size]
-     "text" [:input {:type "text"
-                     :on-change #(rf/dispatch [::events/assoc-shape :text (get-value %)])}]
-     "color" [color-picker]
-     (button #(let [{:keys [x y size color text]} @shape]
-                (let [shape (shapes/text [x y] size color text)]
-                  (rf/dispatch [::events/add-shape shape])
-                  (rf/dispatch [::events/publish-command shape])
-                  (rf/dispatch [::events/input-shape nil])))
-             "Add")]))
+    [:div {:style {:display "flex"}}
+     [span {:margin-right 5} "x"]
+     [num-input :x]
+
+     [span {:margin-left 5, :margin-right 5} "y"]
+     [num-input :y]
+
+     [span {:margin-left 5, :margin-right 5} "size"]
+     [num-input :size]
+
+     [span {:margin-left 5, :margin-right 5} "text"]
+     [:input {:type "text"
+              :placeholder "Hello, world!"
+              :on-change #(rf/dispatch [::events/assoc-shape :text (get-value %)])}]
+
+     [span {:margin-left 5, :margin-right 5} "color"]
+     [color-picker]
+
+     [button {:style {:margin-left "auto"}}
+      #(let [{:keys [x y size color text]} @shape]
+         (dispatch-shape (shapes/text [x y] size color text)))
+      "Add"]]))
